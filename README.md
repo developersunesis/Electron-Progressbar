d512b0b7b515ac325f93b7394a1f700d570c44f5
developersunesis <kingsonesis1@gmail.com>
2019-08-28 10:47:23 +0100

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # Electron-Progressbar
+electronjs-progressbar is a lightweight lib that adds a progressbar to the top of your window view. Progressbar type currently support determinate or intermediate. It is simple and easy to implement and requires only the `body` element of the page present. 
+
+# Installation
+`
+npm i electronjs-progressbar
+`
+or visit https://www.npmjs.com/package/electronjs-progressbar
+
+# Configuration
+`baseBackground`      :     `color (default: transparent)` (This defines the background color of the parent element of the progressbar)
+<br/>
+<br/>
+`progressBackground`  :     `color [optional] (default: blue)` (I really consider blue to be a cool and nice color for the progressbar background)
+<br/>
+<br/>
+`type`                :     `options[ "D", "I"] [optional] (default = "D")`  (Sets the type of progressbar to either `determinate` or `intermediate` using letters `D` or `I` respectively
+<br/>
+<br/>
+`speed`               :     `integer [optional] (default = 3)`   (Sets the progress speed using this attribute)
+<br/>
+<br/>
+`maxProgressWidth`    :     `integer [optional] (default = 20)`  (Sets the max width of the progressbar)
+<br/>
+<br/>
+ `barHeight`          :     `integer [optional] (default = 3)`  (Sets the height of the progressbar) 
+<br/>
+<br/>
+`customProgressCss`           :     `String [optional]` (Sets your custom css for progressbar, for example "margin-top: 10px; width: 50%")
+<br/>
+<br/>
+`customCss`           :     `String [optional]` (Sets your custom css for progressbar's container, for example "margin-top: 10px; width: 50%")
+
+# Properties
+`progressbar.COMPLETED`     `Equals to "C" and triggers progressbar completion`
+<br/>
+<br/>
+`progressbar.LOADING`       `Equals to "L" and sets progressbar state as loading`
+<br/>
+<br/>
+`progressbar.progressBarID`       `Gets progressbar's id usually "electronjsprogressbar"`
+
+# Usage
+
+    //import the library
+    const createProgressBar = require('electronjs-progressbar');
+
+    //initialize progressbar
+    const config = {
+
+          }
+          
+    var progressbar = createProgressBar({
+          config
+          });
+
+    /**
+    * or inline initialize
+    * var progressbar = createProgressBar({
+    *       
+    *    });
+    **/
+    
+    //start progressbar
+    progressbar.setProgressStatus();
+    
+    //complete progressbar using
+    //progressbar.setProgressStatus("C"); or progressbar.setProgressStatus(progressbar.COMPLETED);
+    //for a live simulation I would call this after 3000 milliseconds to actually show the progressbar loading
+    
+    setTimeout(function(){
+      progressbar.setProgressStatus(progress.COMPLETED);
+    }, 3000);
+
+
--- index.js
+++ index.js
@@ -1,25 +1,59 @@
 'use-strict'
 
+/*
+*   @param config
+*   return Object object
+*/
+
 module.exports = function (config) {
+    
+    /*
+    *   Define and initialise the progressbar types
+    *   D -> determinate progress
+    *   I -> Intermediate progress
+    */
     const type1 = "D";
     const type2 = "I";
     
+    //sets a default progressbar max width
     let Imax = 20;
-    const customCSS = "";
 
+    //sets user custom css if != ""
+    const customCSS = "";
+    const customProgressCSS = "";
+
+    //if user initiates without a custom configuration use this
+    //else use user's configuration
     if(config == null){
+        
+        //new config object
         config = {};
 
-        config.baseBackground = "pink";
-        config.progressBackground = "green";
+        //sets progressbar parent's background
         config.baseBackground = "#00000000";
+
+        //sets progressbar background color
         config.progressBackground = "blue";
+
+        //sets progress type [D or I]
         config.type = type1;
-        config.speed = 2;
+
+        //sets progressbar speed
+        config.speed = 3;
+
+        //sets a max width for progressbar
         config.maxBarWidth = 20;
+
+        //sets the progressbar height
         config.barHeight = 3;
 
+        //sets custom container
+        config.container = "body";
     } else {
+        
+        //sets custom value for the config elements
+        if(config.container == null) config.container = "body";
+
         if(config.baseBackground == null) config.baseBackground = "#00000000";
         
         if(config.progressBackground == null) config.progressBackground = "blue";
@@ -33,20 +67,14 @@ module.exports = function (config) {
         if(config.barHeight == null) config.barHeight = 5;
 
         if(config.customCSS != null) customCSS = config.customCSS;
+
+        if(config.customProgressCSS != null) customProgressCSS = config.customProgressCSS;
     }
 
+    //create an object
     const obj = {};
 
-    obj.LOADING = "L";
-    obj.COMPLETED = "C";
-    
-    if(obj.status == null)
-        obj.status = "L";
-
-    const body = document.querySelector("body");
-    let div = document.createElement("div");
-    let progress = document.createElement("div");
-
+    //Assigns variable 
     const type = config.type;
     const speed = config.speed;
     const barHeight = config.barHeight;
@@ -54,49 +82,107 @@ module.exports = function (config) {
     let end = false;
     let progressLevel = 0;
 
+    //set default object properties that can be used
+    //LOADING states that the progressbar is still working
+    obj.LOADING = "L";
+    //COMPLETED initiate a progress completion of the progressbar
+    obj.COMPLETED = "C";
+    
+    //if status is null sets the default to LOADING
+    if(obj.status == null)
+        obj.status = "L";
+
+    //Gets the document's body element
+    const body = document.querySelector(container);
+    
+    //sets margin of the container to 0px
+    body.style.margin = "0px";
+
+    //Creates two new div (parent div and progressbar div)
+    let div = document.createElement("div");
+    let progress = document.createElement("div");
+
+    //defines parent div style
     div.style.width = "100%";
     div.style.height = barHeight+"px";
     div.style.background = config.baseBackground;
     div.style.position = "absolute";
     div.style.top = 0;
 
+    //sets user custom css if != ""
+    if(customCSS != ""){
+        customCSS += div.style.cssText;
+        div.style.cssText = customCSS;
+    }
+
+    //defines progressbar div style
     progress.style.width = "0%";
     progress.style.height = barHeight+"px";
     progress.style.background = config.progressBackground;
     progress.style.position = "absolute";
     progress.style.top = 0;
     progress.style.left = "0%";
-    progress.id = "progressbar";
+    progress.id = "electronjsprogressbar";
     
-    if(customCSS != ""){
-        customCSS += progress.style.cssText;
-        progress.style.cssText = customCSS;
+    //sets user custom css if != ""
+    if(customProgressCSS != ""){
+        customProgressCSS += progress.style.cssText;
+        progress.style.cssText = customProgressCSS;
     }
 
+    //append progress div to parent div
     div.appendChild(progress);
     
-    body.style.margin = "0px";
+    //append parent div to container|body
     body.appendChild(div);
 
-    obj.setProgress = (progressLevel) => {
+    //set configuration
+    obj.config = config;
 
-        const progress = document.querySelector("#progressbar");
+    //set progressbar ID
+    obj.progressBarID = progress.id;
+
+    /*
+    *   sets progressbar progress
+    *   @param progressLevel [integer
+    */
+    obj.setProgress = (progressLevel) => {
+        //obtain progress element
+        //get and initialize some of its properties width and left
+        const progress = document.querySelector("#electronjsprogressbar");
         const currentWidth = progress.style.width;
         const leftSpace = progress.style.left;
 
+        //parseInt the properties currentWidth and leftSpace
         const length = parseInt(currentWidth.toString().replace("%", ""));
         const left = parseInt(leftSpace.toString().replace("%", ""));
+
+        //initialize lSpace
         let lSpace = (left !== 80 && left < 81) ? (left + speed) : 80;
 
+        //if type is equal to determinate 'D'
         if(type == type1){
             if(progressLevel == null){
+                
+                //Ops you have refused to provide a progress for us
                 console.error("ProgressBar: Param progressLevel not set in setProgress() or change type to " +type2);
+            
             } else {
+                
+                //continue progressing except level is equal to 100%
                 if(currentWidth !== "100%"){
                     progress.style.width = progressLevel+"%";
                 }
             }
-        } else {                        
+        } else { 
+
+            //type is set a intermediate
+            //if: increase the progressbar to Imax if progressbar width != Imax
+            //else if: move the progressbar by an incremental left
+            //else if: if progressbar has reached maz parent position, reduce progressbar width to approach 0
+            //and increase left to approch 100
+            //else if: progressbar is equal to 0 restart by setting left to 0
+
             if(length < Imax && left < 80){
                 innerProgress = length + speed;
                 progress.style.width = innerProgress+"%";
@@ -119,20 +205,29 @@ module.exports = function (config) {
                     end = false;
                 }
             } else if(currentWidth == 0){
-                progress.style.left = 0;
+                progress.style.left = 0+"0";
             }
             
         }
     }
 
+    //This is a simple setInterval function to rerun the progress 200 ms
+    //used instead of a loop and thread sleep
     obj.progresser = setInterval( function(){
+
+        //if: status is LOADING and type != I setProgress in a determinate method
+        //else if: LOADING && I set progress in a intermediate method
+        //else if: COMPLETED turn of progress and set status to DONE
+
         if(obj.status === "L" && progressLevel < 81){
             progress.style.display = "block";
             obj.setProgress(progressLevel);
+            
             progressLevel += speed;
         } else if(obj.status === "L" && type === "I"){
             progress.style.display = "block";
             obj.setProgress(progressLevel);
+
             progressLevel += speed;  
         } else if(obj.status == "C") {
             if(progressLevel < 80) progressLevel = 85;
@@ -147,31 +242,50 @@ module.exports = function (config) {
         
     }, 200);
 
+    /*
+    * Set progress status
+    * @param status (default = "L")
+    */
     obj.setProgressStatus = (status = "L") => {
-        const progress = document.querySelector("#progressbar");
+        //obtain progress element
+        const progress = document.querySelector("#electronjsprogressbar");
                         
         if(status === "C"){
+            //triggers a completion
             obj.status = "C";
         } else if(status === "D"){
+            //create setInterval
             clearInterval(obj.progresser);
+            
+            //reset elements properties once DONE : D
             progress.style.display = "none";
             progress.style.left = "0%";
             progress.style.width = "0%";
             progressLevel = 0;
         }
 
+        //starts a new progress if was recently completed 
         if(status === "L" && obj.status == "C"){
             obj.status = "L";
             progress.style.display = "block";
 
+            //This is a simple setInterval function to rerun the progress 200 ms
+            //used instead of a loop and thread sleep
             obj.progresser = setInterval( function(){
+
+                //if: status is LOADING and type != I setProgress in a determinate method
+                //else if: LOADING && I set progress in a intermediate method
+                //else if: COMPLETED turn of progress and set status to DONE
+
                 if(obj.status === "L" && progressLevel < 81){
                     progress.style.display = "block";
                     obj.setProgress(progressLevel);
+                    
                     progressLevel += speed;
                 } else if(obj.status === "L" && type === "I"){
                     progress.style.display = "block";
                     obj.setProgress(progressLevel);
+
                     progressLevel += speed;  
                 } else if(obj.status == "C") {
                     if(progressLevel < 80) progressLevel = 85;
@@ -179,7 +293,6 @@ module.exports = function (config) {
                     obj.setProgress(progressLevel);
 
                     if(progressLevel > 100){
-                        console.log("Done");
                         obj.setProgressStatus("D");
                     }
                     progressLevel += speed;
@@ -190,7 +303,6 @@ module.exports = function (config) {
 
     }
 
-    obj.config = config;
-    obj.progressBarID = progress.id;
+    //return object
     return obj;
 }
